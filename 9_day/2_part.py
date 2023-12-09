36bac3ba3c30da124eb48614fd915ae034fea05c
NikolasMichalicka <michalickanikolas@gmail.com>
2023-12-09 23:30:11 +0100

    day 9
    advent of code day 9 completed

---
+++ 9_day/2_part.py
@@ -0,0 +1,30 @@
+import time
+
+start_time = time.perf_counter()
+file = open("input.txt", "r")
+lines = file.readlines()
+
+ans = 0
+for index, line in enumerate(lines):
+    line = list(map(int, line.split(" ")))
+    arrs = [line]
+    print(f'({index+1}/{len(lines)})')
+    print(line)
+    while True:
+        arr = []
+        last = arrs[-1]
+        for i in range(len(last)-1):
+            arr.append(last[i+1]-last[i])
+        if all(v == 0 for v in arr):
+            break
+        arrs.append(arr)
+        xs = line
+        print(arr)
+    num = 0
+    for arr in arrs[::-1]:
+        num = arr[0]-num
+    print(num)
+    ans+=num
+
+print(ans)
+print(f"Part2 done in {(time.perf_counter()-start_time):02f} seconds")
