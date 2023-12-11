90f70c16a4742673ef3cbd023f2976f9fb8a9587
NikolasMichalicka <michalickanikolas@gmail.com>
2023-12-11 22:21:47 +0100

    day 10 part 1
    part 1 completed

--- 10_day/1_part.py
+++ 10_day/1_part.py
@@ -4,33 +4,37 @@ start_time = time.perf_counter()
 file = open("input.txt", "r")
 lines = file.readlines()
 
-pipes = ['|','L','J','7','F','-']
-
-def has_pipes(pipe):
-    if pipe == '|':
-        return []
-
+pipes = {'S':['up','down','left','right'],'|':['up','down'],'L':['up', 'right'],'J':['up', 'left'],'7':['down', 'left'],'F':['down', 'right'],'-':['left', 'right'],'.':[]}
 
 def has_neighbours(pos):
-    print(f'Searching neighbours for {pos}')
+    #print(f'Searching neighbours for {pos}')
     x, y = pos
     buf = []
     a_pos, b_pos, c_pos, d_pos = [x-1,y],[x+1,y],[x,y-1],[x,y+1]
-    a,b,c,d = arr[y][x-1],arr[y][x+1],arr[y-1][x],arr[y+1][x]
-    print(a,b,c,d)
-    if not a_pos in used and a in pipes and a_pos[0]>=0:
+    a,b,c,d = 4*['.']#arr[y][x-1],arr[y][x+1],arr[y-1][x],arr[y+1][x]
+    if x-1 >= 0:
+        a = arr[y][x-1]
+    if x+1 < len(arr[0]):
+        b = arr[y][x+1]
+    if y-1 >= 0:
+        c = arr[y-1][x]
+    if y+1 < len(arr):
+        d = arr[y+1][x]
+    pipe = pipes[arr[y][x]]
+    #print(a,b,c,d)
+    if not a_pos in used and 'left' in pipe and 'right' in pipes[a]:
         buf.append(a_pos)
         used.append(a_pos)
-    if not b_pos in used and b in pipes and b_pos[0]<=len(arr[0])-1:
+    if not b_pos in used and 'right' in pipe and 'left' in pipes[b]:
         buf.append(b_pos)
         used.append(b_pos)
-    if not c_pos in used and c in pipes and c_pos[1]>=0:
+    if not c_pos in used and 'up' in pipe and 'down' in pipes[c]:
         buf.append(c_pos)
         used.append(c_pos)
-    if not d_pos in used and d in pipes and d_pos[1]<=len(arr)-1:
+    if not d_pos in used and 'down' in pipe and 'up' in pipes[d]:
         buf.append(d_pos)
         used.append(d_pos)
-    print(f'Found {buf}')
+    #print(f'Found {buf}')
     return buf
 
 start = []
@@ -41,10 +45,10 @@ for  idx,line in enumerate(lines):
         start = [line.index('S'), idx]
     arr.append(line.strip('\n'))
 
-print(start)
 ans = 0
 neighbours = [has_neighbours(start)]
 while True:
+    #print(f'# {ans}')
     neighbours_new = []
     for neighbour in neighbours:
         for neigh in neighbour:
