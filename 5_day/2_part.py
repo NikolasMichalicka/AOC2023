388d94bb3d2c6c5b92061f8d5cd8e319d4900d99
NikolasMichalicka <michalickanikolas@gmail.com>
2023-12-15 14:07:12 +0100

    update day 5 part 2
    unfinished 5 day

--- 5_day/2_part.py
+++ 5_day/2_part.py
@@ -16,12 +16,25 @@ def getMap():
         buf.append(list(map(int, str.split())))
     return buf
 
-def convertSeed(seed, seed_map):
+def converter(input_list, output_list):
+    for elements in input_list:
+        if type(elements) == list:
+            converter(elements,output_list)
+        else:
+            output_list.append(elements)
+    return output_list
+
+def convertSeed(interval, seed_map):
+    if interval[0] == interval[1]:
+        return
+    seed = interval[0]
     for x in seed_map:
         if seed >= x[1] and seed < x[1]+x[2]:
             seed += x[0]-x[1]
-            return seed
-    return seed
+            seed1 = min(x[1]+x[2]-1,interval[1]) + x[0]-x[1]
+            interval = (min(x[1]+x[2],interval[1]),interval[1])
+            return [[seed,seed1], convertSeed(interval, seed_map)]
+    return [interval[0], interval[1]]
 
 
 seeds =  list(map(int, lines[0].split(":")[1].strip(" ").split()))
@@ -35,17 +48,30 @@ temperature_to_humidity = getMap()
 humidity_to_location = getMap()
 answer = None
 
+intervals = []
 for i in range(0,len(seeds),2):
-    for j in range(seeds[i+1]):
-        seed = seeds[i]+j
-        seed = convertSeed(seed, seed_to_soil)
-        seed = convertSeed(seed, soil_to_fertilizer)
-        seed = convertSeed(seed, fertilizer_to_water)
-        seed = convertSeed(seed, water_to_light)
-        seed = convertSeed(seed, light_to_temperature)
-        seed = convertSeed(seed, temperature_to_humidity)
-        seed = convertSeed(seed, humidity_to_location)
-        if answer == None or seed < answer:
-            answer = seed
+    intervals.append((seeds[i],seeds[i]+seeds[i+1]-1))
+
+for seed_map in [seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location]:
+    new_intervals = []
+    #print(intervals)
+    while any(intervals):
+        interval = intervals[0]
+        L = list(convertSeed(intervals[0], seed_to_soil))
+        #L = converter(L)
+        #print(L)
+        if type(L[0]) != list:
+            L = [[L[0],L[1]]]
+        L = converter(L, [])
+        #print(L)
+        for i in range(0,len(L)-1,2):
+            new_intervals.append((L[i],L[i+1]))
+    
+        intervals.pop(0)
+    for x in new_intervals:
+        intervals.append(x)
+for x in intervals:
+    if answer == None or answer > x[0]:
+        answer = x[0]
 
 print(answer)
